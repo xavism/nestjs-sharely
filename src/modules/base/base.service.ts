ad8fde3527ee85897ade5a1835b651a460027286
Xavi Sánchez Mir <xavi.sanchez.mir@gmail.com>
2018-03-02 12:25:22 +0100

    Addings exceptions

--- src/modules/base/base.service.ts
+++ src/modules/base/base.service.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@nestjs/common';
+import { Component, Inject, BadGatewayException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { STATUS_CODES } from 'http';
@@ -13,41 +13,62 @@ export class BaseService<T extends BaseEntity> implements IBaseService<T>{
 	}
 
   create(entity: T): Promise<number>{
+	  try {
 		return new Promise<number> ((resolve, reject) => {
-		
-		this.genericRepository.save(entity)
-		.then(created=> resolve(created.id))
-		.catch(err => reject(err))
-		})
+			this.genericRepository.save(entity)
+			.then(created=> resolve(created.id))
+			.catch(err => reject(err))
+			})
+		}
+		catch(error) {
+			throw new BadGatewayException(error);
+		}
   }
 
   getAll(): Promise<T[]> {
-    return <Promise<T[]>>this.genericRepository.find();
+	  try {
+		return <Promise<T[]>>this.genericRepository.find();
+	  } catch (error) {
+		throw new BadGatewayException(error);
+	}
   }
 
   get(id: number): Promise<T> {
+	try {
+		  
+	} catch (error) {
+		throw new BadGatewayException(error);
+	}
   	return <Promise<T>>this.genericRepository.findOneById(id);
   }
 
   delete(id: number) {
-  	this.genericRepository.deleteById(id)
+	try {
+		this.genericRepository.deleteById(id)
+	} catch (error) {
+		throw new BadGatewayException(error);
+	}
   }
 
   update(entity: T): Promise<T>{
+	try {
 		return new Promise<T> ((resolve, reject) => {
-		this.genericRepository.findOneById(entity.id)
-		.then(responseGet => {
-			try {
-				if (responseGet == null) reject('Not existing')
-				this.genericRepository.save(responseGet)
-				.then(response => resolve(response))
-				.catch(err => reject(err))
-			}
-			catch(e) {
-					reject(e)
-			}
-		})
-		.catch(err => reject(err))
-		})
+			this.genericRepository.findOneById(entity.id)
+			.then(responseGet => {
+				try {
+					if (responseGet == null) reject('Not existing')
+					this.genericRepository.save(responseGet)
+					.then(response => resolve(response))
+					.catch(err => reject(err))
+				}
+				catch(e) {
+						reject(e)
+				}
+			})
+			.catch(err => reject(err))
+			}) 
+	} catch (error) {
+		throw new BadGatewayException(error);
+	}
   }
 }
